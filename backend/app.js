c1346d9c7d994ee302ed50d9da546849dce958c6
zarjin-404 <zarjinislamjewel@gmail.com>
2025-01-24 20:32:11 +0600

    add

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ try {
   console.log('Database connected successfully');
 } catch (error) {
   console.error('Database connection failed:', error);
-  process.exit(1); // Exit process with failure code
+  process.exit(1);
 }
 
 app.use(
